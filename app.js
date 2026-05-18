86236a323ed8238631f5e070fd796ffff108efb3
perodman <per.odman@hotmail.com>
2026-05-18 13:05:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -610,7 +610,7 @@ function openDayManager(dateStr, planned, completed, isOngoing) {
 
         // ÄNDRA PLANERING - SEKTIONSAVSKILJARE 
         html += `
-        <div style="margin-top: 10px;">
+        <div style="margin-top: 30px;">
             <div style="display: flex; align-items: center; gap: 10px; margin-bottom: 12px;">
                 <div style="flex-grow: 1; height: 1px; background: rgba(255,255,255,0.08);"></div>
                 <p style="font-size: 14px; text-transform: uppercase; color: var(--text-light); font-weight: 700; letter-spacing: 1px; margin: 0;">Ändra planering</p>
